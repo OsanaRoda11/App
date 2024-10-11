91db990f7977b2a3437f490aad43ca9e004a3485
OsanaRoda11 <osanaroda00@gmail.com>
2024-10-11 17:46:41 +0100

    criacao do menu

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 console.log('Hello word');
+const {select} = require('@inquirer/prompts')
+
+const start = async () => {
+    
+    while(true){
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {name: "Cadastrar meta", value: "cadastrar"},
+                {name: "Listar metas", value: "listar"},
+                {name: 'Sair', value:'sair'}
+            ]
+        })
+        switch(opcao){
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")  
+                break
+            case "sair": 
+                console.log("Sair")
+                return      
+        }
+    }
+}
+
+start()
